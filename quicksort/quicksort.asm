1e9104605678978abde52149451d0a4604d1cebe
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-01-20 22:26:09 +0100

    Create array with numbers given by the user

--- quicksort/quicksort.asm
+++ quicksort/quicksort.asm
@@ -7,23 +7,147 @@ name "quicksort"
 org 100h
 
 .data
-  prompt db "Enter number: $"
+  array dw ?
+  length dw ?
+  prompt_length db "Enter length of array: $"
+  prompt_length_invalid db "Invalid length! Must be a positive number!", 10, 13, "$"
+  prompt_number db "Enter number $"
+  prompt_number_end db ": $"
+  msg_array_entered db "The array you entered: $"
   
 .code
-  ; ask user for number
-  lea dx, prompt
+  ; ask user for the length of the array
+  START_LENGTH_PROMPT:
+    lea dx, prompt_length
+    mov ah, 9
+    int 21h
+    
+    ; get length
+    call get_num
+    mov length, dx
+    
+    call print_newline
+    
+    ; if length <= 0, re-ask user for length
+    cmp length, 0
+    jg END_LENGTH_PROMPT
+      lea dx, prompt_length_invalid
+      mov ah, 9
+      int 21h
+      call print_newline
+      jmp START_LENGTH_PROMPT        
+  END_LENGTH_PROMPT:
+  
+  ; allocate space for array
+  mov cx, length
+  ALLOCATE_ARRAY:
+    push 0
+    loop ALLOCATE_ARRAY
+  mov array, sp ; remember: to access element, do array+2i where i is the index
+  
+  ; ask user numbers for array
+  mov cx, 0 ; index for array
+  mov bp, array ; put array address to bp
+  START_NUMBERS_PROMPT:
+    cmp cx, length
+    jge END_NUMBERS_PROMPT
+    lea dx, prompt_number
+    mov ah, 9
+    int 21h
+    mov dx, cx  ; print current index
+    inc dx
+    call print_num
+    lea dx, prompt_number_end
+    mov ah, 9
+    int 21h
+    
+    ; get number
+    call get_num
+    
+    ; calculate actual array index
+    call get_actual_index
+    mov w.[si], dx
+    
+    call print_newline
+    
+    inc cx
+    jmp START_NUMBERS_PROMPT
+  END_NUMBERS_PROMPT:
+  
+  ; Show to user their array
+  lea dx, msg_array_entered
   mov ah, 9
   int 21h
   
-  call get_num
-  push dx          
-  
+  mov bp, array
+  mov cx, length
+  call print_array
   call print_newline
   
-  pop dx
-  call print_num
-  
   .exit
+
+; =============================================================================
+; \brief Function to print array
+; \param cx Size of the array
+; \param bp Base pointer of the array
+print_array proc
+  mov ah, 2
+  mov dl, '['
+  int 21h
+  
+  mov ax, cx ; move length to ax register                
+  mov cx, 0 ; cx is the index of the current element
+  START_PRINT_ARRAY:
+    cmp cx, ax
+    jge END_PRINT_ARRAY
+    
+    call get_actual_index
+    mov dx, w.[si]  ; get current element to dx
+    call print_num
+    
+    ; print comma
+    push ax
+    mov dx, ax
+    dec dx
+    cmp cx, dx ; where dx = length - 1
+    jge SKIP_COMMA_PRINT_ARRAY
+      mov dl, ','
+      mov ah, 2
+      int 21h
+      mov dl, ' '
+      int 21h
+    SKIP_COMMA_PRINT_ARRAY:
+    
+    pop ax
+    inc cx
+    jmp START_PRINT_ARRAY
+  END_PRINT_ARRAY:
+  
+  mov ah, 2
+  mov dl, ']'
+  int 21h
+  ret
+print_array endp
+
+; =============================================================================
+; \brief Function to calculate actual index
+; \param bp Base pointer of the array
+; \param cx Index of element to calculate
+; \return si 
+get_actual_index proc
+  push ax ; save ax value
+  push bx ; save bx value
+  
+  mov ax, cx
+  mov bl, 2
+  mul bl
+  add ax, bp
+  mov si, ax
+  
+  pop bx ; restore bx
+  pop ax ; restore ax
+  ret
+get_actual_index endp
 
 ; =============================================================================
 ; \brief Function to print a newline
@@ -43,6 +167,25 @@ print_newline endp
 ; \brief Function to print number
 ; \param DX The number to print
 print_num proc
+  ; handle if number is 0
+  cmp dx, 0
+  jne NOT_ZERO_PRINT_NUM
+    ; print negative symbol
+    mov dl, 48 ; '0'
+    mov ah, 2
+    int 21h
+    ret
+  NOT_ZERO_PRINT_NUM:
+  
+  ; prepare stack for putting in digits
+  push bp ; save bp value
+  push ax ; save ax value
+  push bx ; save bx value
+  push cx ; save cx value
+  push dx ; save dx value
+  mov bp, sp ; move current sp to bp which will be used to know
+             ; where digits start
+  
   ; handle if number is negative
   cmp dx, 0
   jge NOT_NEG_PRINT_NUM
@@ -56,21 +199,6 @@ print_num proc
     pop dx
     neg dx
   NOT_NEG_PRINT_NUM:
-  
-  ; handle if number is 0
-  cmp dx, 0
-  jne NOT_ZERO_PRINT_NUM
-    ; print negative symbol
-    mov dl, 48 ; '0'
-    mov ah, 2
-    int 21h
-    ret
-  NOT_ZERO_PRINT_NUM:
-  
-  ; prepare stack for putting in digits
-  push bp ; save bp value
-  mov bp, sp ; move current sp to bp which will be used to know
-             ; where digits start
              
   ; put each digit to stack           
   mov ax, dx 
@@ -98,7 +226,11 @@ print_num proc
     
     jmp START_PRINT_NUM
   END_PRINT_NUM:
-  
+                     
+  pop dx ; restore dx
+  pop cx ; restore cx
+  pop bx ; restore bx
+  pop ax ; restore ax
   pop bp
   ret
 print_num endp
@@ -108,6 +240,9 @@ print_num endp
 ; \return Number from user saved in DX register
 get_num proc
   push bp ; save bp value
+  push ax ; save ax value
+  push bx ; save bx value
+  push cx ; save cx value
   mov bp, sp ; move current sp to bp which will be used to know
              ; where inputs start
   
@@ -219,7 +354,10 @@ get_num proc
     inc cx
     jmp START_CONVERSION_GET_NUM
   END_CONVERSION_GET_NUM:
-  
+                     
+  pop cx ; restore cx
+  pop bx ; restore bx
+  pop ax ; restore ax
   pop bp ; restore bp
   ret
 get_num endp
