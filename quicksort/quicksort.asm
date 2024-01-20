9ecb6b68c15ab90a87ff6bab4d65bf3a1d334f35
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-01-20 20:01:46 +0100

    Create function to get number from user

---
+++ quicksort/quicksort.asm
@@ -0,0 +1,128 @@
+; Sort Array
+; \desc Asks user for n integer inputs which will be then
+;       sorted using quick sort.
+
+name "quicksort"
+
+org 100h
+
+.data
+  prompt db "Enter number: $"
+  
+.code
+  ; ask user for number
+  lea dx, prompt
+  mov ah, 9
+  int 21h
+  
+  call get_num
+  
+  .exit
+
+; =============================================================================
+; /brief Function to get number input
+get_num proc
+  push bp ; save bp value
+  mov bp, sp ; move current sp to bp which will be used to know
+             ; where inputs start
+  
+  START_GET_NUM:
+    mov ah, 7 ; get char input without echo
+    int 21h
+    
+    ; if char == 'Enter' then stop input
+    cmp al, 10 ; if char == '\n'
+    je END_GET_NUM
+    cmp al, 13 ; if char == '\r'
+    je END_GET_NUM
+    
+    ; prevent '-' inputs if not start of number
+    cmp al, 45
+    jne SKIP_NEG_HANDLER_GET_NUM
+      cmp bp, sp
+      jne START_GET_NUM
+    SKIP_NEG_HANDLER_GET_NUM:
+    
+    ; handle backspace input
+    cmp al, 8 ; if char != '\b'
+    jne SKIP_BACKSPACE_HANDLER_GET_NUM
+      cmp bp, sp ; if there's no input yet then skip backspace
+      je SKIP_BACKSPACE_HANDLER_GET_NUM
+    
+      ; remove char digit from stack
+      pop ax
+      
+      ; print backspace
+      mov dl, 8
+      mov ah, 2
+      int 21h   ; move cursor back
+      mov dl, 0
+      mov ah, 2
+      int 21h   ; print null to replace whatever char there was
+      mov dl, 8
+      mov ah, 2
+      int 21h   ; move cursor back again  
+    SKIP_BACKSPACE_HANDLER_GET_NUM:
+    
+    ; char must be between 0 and 9 and '-'
+    cmp al, 45
+    je MINUS_SIGN_GET_NUM
+    cmp al, 48 ; if char < '0'
+    jl START_GET_NUM
+    cmp al, 57 ; if char > '9'
+    jg START_GET_NUM
+    MINUS_SIGN_GET_NUM:
+    
+    ; char is a digit, echo it
+    mov dl, al
+    mov ah, 2
+    int 21h
+    
+    ; push char digit
+    mov ah, 0
+    push ax
+    
+    jmp START_GET_NUM
+  END_GET_NUM:
+  
+  ; convert char digits to number from stack
+  mov dx, 0 ; store number in dx
+  mov cx, 0 ; cx is the digit place (tenth, hundreds, etc.)
+  START_CONVERSION_GET_NUM:
+    cmp bp, sp
+    je END_CONVERSION_GET_NUM
+    pop ax
+    
+    ; handle negative '-'
+    cmp ax, 45
+    jne NOT_NEGATIVE_INPUT_GET_NUM
+      neg dx
+      jmp END_CONVERSION_GET_NUM ; exit loop since '-' means end anyway
+    NOT_NEGATIVE_INPUT_GET_NUM:
+    
+    ; convert digit char to digit
+    sub ax, 48
+    
+    ; get the digit's place by multiplying by 10
+    ; each time for up to cx (the current digit place)
+    push cx
+    START_PLACE_DIGIT_GET_NUM:
+      cmp cx, 0
+      je END_PLACE_DIGIT_GET_NUM 
+      mov bl, 10
+      mul bl
+      loop START_PLACE_DIGIT_GET_NUM
+    END_PLACE_DIGIT_GET_NUM:
+    pop cx
+    
+    ; finally add the digit number to the actual number
+    add dx, ax
+    
+    inc cx
+    jmp START_CONVERSION_GET_NUM
+  END_CONVERSION_GET_NUM:
+  
+  pop bp ; restore bp
+  ret
+get_num endp
+                                                                                        
