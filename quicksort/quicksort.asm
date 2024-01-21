5a14af7f89009620dd8e280430fcb09c5c7467e9
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-01-21 03:42:59 +0100

    Create quicksort

--- quicksort/quicksort.asm
+++ quicksort/quicksort.asm
@@ -1,6 +1,8 @@
 ; Sort Array
 ; \desc Asks user for n integer inputs which will be then
 ;       sorted using quick sort.
+; \author Zekumoru
+; \version 1.0.240121
 
 name "quicksort"
 
@@ -14,8 +16,10 @@ org 100h
   prompt_number db "Enter number $"
   prompt_number_end db ": $"
   msg_array_entered db "The array you entered: $"
+  msg_sort_loading db "Sorting array...", 10, 13, "$"
+  msg_array_sorted db "The array sorted: $"
   
-.code
+.code    
   ; ask user for the length of the array
   START_LENGTH_PROMPT:
     lea dx, prompt_length
@@ -84,12 +88,191 @@ org 100h
   call print_array
   call print_newline
   
+  ; Sort array
+  lea dx, msg_sort_loading
+  mov ah, 9
+  int 21h
+  call quicksort
+  
+  ; Show sorted array to user
+  lea dx, msg_array_sorted
+  mov ah, 9
+  int 21h
+  
+  mov bp, array
+  mov cx, length
+  call print_array
+  call print_newline 
+  
   .exit
+
+; =============================================================================
+; \brief Sort array using quicksort
+; \param cx Size of the array
+; \param bp Base pointer of the array
+quicksort proc
+  push ax ; save ax value
+  push bx ; save bx value
+  push dx ; save dx value
+  
+  mov ax, 0
+  mov bx, cx
+  dec bx
+  call quicksort_int
+  
+  pop dx ; restore dx
+  pop bx ; restore bx
+  pop ax ; restore ax
+  ret
+quicksort endp
+
+; \brief Internal quicksort helper
+; \param bp Base pointer of the array
+; \param ax Start index
+; \param bx End index
+quicksort_int proc
+  ; if (start >= end) return
+  cmp ax, bx
+  jl NO_EARLY_RET_QUICKSORT
+    ret
+  NO_EARLY_RET_QUICKSORT:
+  
+  ; local variables
+  mov si, bp ; since bp has address of array, move it to si
+  mov bp, sp ; get local variables' base address
+  push si ; address of array [bp-2], this will be restored as bp later
+  push ax ; start index [bp-4], this will be restored after function call
+  push bx ; end index [bp-6], this will be restored after function call
+  push cx ; save cx value
+  
+  ; get mid index to ax
+  ; mid = (start + end) / 2
+  mov dx, 0
+  add ax, bx
+  shr ax, 1 ; divide by 2
+  
+  ; get pivot to dx
+  push bp
+  mov bp, w.[bp-2] ; get array address to bp
+  mov cx, ax
+  call get_actual_index
+  mov dx, w.[si] ; pivot = array[mid] 
+  pop bp ; restore to local vars' address
+  
+  ; partition(array, start, end, pivot)
+  ; |= partition(bp, ax, bx, dx)
+  push bp
+  mov ax, w.[bp-4] ; get start index  
+  mov bx, w.[bp-6] ; get end index
+  mov bp, w.[bp-2] ; get array address to bp
+  call partition   
+  
+  ; recurse left partition
+  ; ax and bx still hold start and end indices respectively
+  ; because the partition function restores them
+  ; quicksort_int(array, start, index - 1)
+  ; |= quicksort_int(bp, ax, cx-1)
+  mov dx, cx ; move index to dx
+  dec dx ; index--
+  push bx ; save bx which contains end
+  mov bx, dx
+  call quicksort_int
+  pop bx  ; restore end to bx
+
+  ; recurse right partition
+  ; ax and bx still hold start and end indices respectively
+  ; because ax is restored by the partition function and
+  ; bx is saved from the stack
+  ; quicksort_int(array, index, end)
+  ; |= quicksort_int(bp, cx, bx)
+  mov ax, cx ; set ax to index
+  call quicksort_int
+                            
+  pop bp ; restore to local vars' address
+  
+  pop cx ; restore cx
+  pop bx ; restore bx
+  pop ax ; restore ax
+  pop bp ; restore original bp
+  
+  ret
+quicksort_int endp
+
+; =============================================================================
+; \brief Partition array using Hoare's algorithm which splits the array into
+;        two parts where elements less than the pivot are all on the left 
+;        and vice versa
+; \param bp Base address of the array
+; \param ax Left pointer
+; \param bx Right pointer
+; \param dx Pivot
+; \return cx The index where to split the array
+partition proc
+  push ax ; save ax value
+  push bx ; save bx value
+  
+  ; while (lp <= rp)
+  START_PARTITION:
+    cmp ax, bx        
+    jg END_PARTITION
+    
+    ; while (array[lp] < pivot) lp++;
+    WHILE_LT_PIVOT_PARTITION:
+      mov cx, ax
+      call get_actual_index
+      cmp w.[si], dx ; array[lp] < pivot
+      jge NOT_LT_PIVOT_PARTITION
+        inc ax ; lp++
+        jmp WHILE_LT_PIVOT_PARTITION
+    NOT_LT_PIVOT_PARTITION:
+    
+    ; while (array[rp] > pivot) rp--;
+    WHILE_GT_PIVOT_PARTITION:
+      mov cx, bx
+      call get_actual_index
+      cmp w.[si], dx ; array[rp] > pivot
+      jle NOT_GT_PIVOT_PARTITION
+        dec bx ; rp--
+        jmp WHILE_GT_PIVOT_PARTITION
+    NOT_GT_PIVOT_PARTITION:
+    
+    ; if (lp <= rp) then swap array[lp] and array[rp]
+    cmp ax, bx
+    jg NO_SWAP_PARTITION
+      ; push array[lp] for temp storage
+      mov cx, ax
+      call get_actual_index
+      push w.[si]
+      ; array[lp] <- array[rp]
+      mov cx, bx
+      call get_actual_index
+      push w.[si]
+      mov cx, ax
+      call get_actual_index
+      pop w.[si]
+      ; array[rp] <- temp (from stack)
+      mov cx, bx
+      call get_actual_index
+      pop w.[si]
+      
+      ; increment lp and decrement rp
+      inc ax ; lp++
+      dec bx ; rp--
+    NO_SWAP_PARTITION:
+    
+    jmp START_PARTITION
+  END_PARTITION:
+  
+  mov cx, ax ; move lp to cx (which is the index)
+  pop bx ; restore bx
+  pop ax ; restore ax
+  ret
+partition endp
 
 ; =============================================================================
 ; \brief Function to print array
 ; \param cx Size of the array
-; \param bp Base pointer of the array
+; \param bp Base pointer of the array       
 print_array proc
   mov ah, 2
   mov dl, '['
